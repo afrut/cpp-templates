e10a81d734f248efc664b7b7cabc7112498322d2
eric373 <ericryanco@gmail.com>
2021-06-20 15:11:53 -0400

    Completed initial implementation

--- patternState/Tank.cpp
+++ patternState/Tank.cpp
@@ -10,6 +10,7 @@ using namespace std;
 Tank::Tank() :
      m_tempMin(45)
     ,m_tempMax(60)
+    ,m_amt(0)
     ,m_level(min_level)
     ,m_temp(min_temp)
 {
@@ -21,110 +22,189 @@ Tank::Tank() :
 Tank::~Tank() {delete m_state;}
 
 // delegate the fill operation to whatever state is active at the time
-void Tank::fill(int amt, int temp)
+int Tank::fill(int amt, int temp)
 {
-    int overflow = amt;
-    while(overflow > 0) overflow = m_state->fill(overflow, temp);
+    m_amt = amt;
+    while(m_amt > 0)
+    {
+        State* newState = m_state->fill(temp);
+        if(newState) this->changeState(newState);
+        cout << "  Tank temp = " << this->temp() << "\n";
+        if(m_state->toString() == std::string("Full"))
+            break;
+    }
+    amt = m_amt;
+    m_amt = 0;
+    return amt;
 }
 
 // delegate the string representation to whatever state is active at the time
 string Tank::toString() {return m_state->toString();}
 
-void Tank::updateState()
+void Tank::changeState(State* newState)
 {
-    // the order in which the conditionals are checked here imply
-    // a priority order between the states
-    if(m_level <= min_level)
-    {
-        m_state->exit();
-        delete m_state;
-        m_state = (State*)new Empty(*this);
-        m_state->enter();
-    }
-    else if(m_temp < m_tempMin)
-    {
-        m_state->exit();
-        delete m_state;
-        m_state = (State*) new Cold(*this);
-        m_state->enter();
-    }
-    else if(m_temp > m_tempMax)
-    {
-        m_state->exit();
-        delete m_state;
-        m_state = (State*) new Hot(*this);
-        m_state->enter();
-    }
-    else
-    {
-        m_state->exit();
-        delete m_state;
-        m_state = (State*) new Normal(*this);
-        m_state->enter();
-    }
+    m_state->exit();
+    delete m_state;
+    m_state = newState;
+    newState->enter();
 }
 
 // ----------------------------------------------------------------------
 // Class Tank::Empty
 // ----------------------------------------------------------------------
-int Tank::Empty::fill(int amt, int temp)
+Tank::State* Tank::Empty::fill(int temp)
 {
     m_context.m_level++;
-    amt--;
+    m_context.m_amt--;
     m_context.m_temp = temp;
-    m_context.updateState();    // as soon as m_level > 0, tank state should change
-    return amt;
+    return this->transition();
+}
+
+Tank::State* Tank::Empty::transition()
+{
+    // Empty can only transition to Hot, Cold or Normal
+    if(m_context.m_level > 0)
+    {
+        if(m_context.m_temp < m_context.m_tempMin)
+            return (State*)new Tank::Cold(m_context);
+        else if(m_context.m_temp > m_context.m_tempMax)
+            return (State*)new Tank::Hot(m_context);
+        else
+            return (State*)new Tank::Normal(m_context);
+    }
+    return NULL;
 }
 
 void Tank::Empty::enter()
 {
     cout << "Tank is " << this->toString() << "\n";
+    // whenever tank is empty, automatically open the hatch
+    m_context.m_hatchOpen = true;
 }
 
 void Tank::Empty::exit()
-{}
+{
+    // whenever tank has contents, automatically close hatch
+    m_context.m_hatchOpen = false;
+}
+
+// ----------------------------------------------------------------------
+// Class Tank::Full
+// ----------------------------------------------------------------------
+Tank::State* Tank::Full::fill(int temp) {return this->transition();}
+Tank::State* Tank::Full::transition()
+{
+    // Full can only transition to Normal
+    if(m_context.m_level < max_level)
+        return (State*)new Tank::Normal(m_context);
+    return NULL;
+}
+
+void Tank::Full::enter() {cout << "Tank is " << this->toString() << "\n";}
+void Tank::Full::exit() {}
 
 // ----------------------------------------------------------------------
 // Class Tank::Cold
 // ----------------------------------------------------------------------
-int Tank::Cold::fill(int amt, int temp)
+Tank::State* Tank::Cold::fill(int temp)
 {
-    return 0;
+    m_context.m_temp = (m_context.m_temp * m_context.m_level) + temp + 1;   // heating should be on
+    m_context.m_level++;
+    m_context.m_temp /= m_context.m_level;
+    m_context.m_amt--;
+    if(m_context.m_amt == 0 || m_context.m_level == max_level)
+        // done filling or tank is full, heat until within limits
+        this->fix();
+    return this->transition();
+}
+
+Tank::State* Tank::Cold::transition()
+{
+    // Cold can only transition to Normal
+    if(m_context.m_temp > m_context.m_tempMin)
+        return (State*)new Tank::Normal(m_context);
+    return NULL;
+}
+
+void Tank::Cold::fix()
+{
+    m_context.m_temp = (m_context.m_tempMin + m_context.m_tempMax) / 2;
+    this->transition();
 }
 
 void Tank::Cold::enter()
 {
     cout << "Tank is " << this->toString() << "\n";
+    m_context.m_heatOn = true;
 }
 
-void Tank::Cold::exit() {}
+void Tank::Cold::exit()
+{
+    m_context.m_heatOn = false;
+}
 
 // ----------------------------------------------------------------------
 // Class Tank::Hot
 // ----------------------------------------------------------------------
-int Tank::Hot::fill(int amt, int temp)
+Tank::State* Tank::Hot::fill(int temp)
 {
-    return 0;
+    m_context.m_temp = (m_context.m_level * m_context.m_temp) + temp - 1;
+    m_context.m_level++;
+    m_context.m_temp /= m_context.m_level;
+    m_context.m_amt--;
+    return this->transition();
+}
+
+Tank::State* Tank::Hot::transition()
+{
+    if(m_context.m_temp < m_context.m_tempMax)
+        return (State*)new Tank::Normal(m_context);
+    return NULL;
+}
+
+void Tank::Hot::fix()
+{
+    m_context.m_temp = (m_context.m_tempMin + m_context.m_tempMax)/2;
 }
 
 void Tank::Hot::enter()
 {
     cout << "Tank is " << this->toString() << "\n";
+    m_context.m_coolOn = true;
 }
 
-void Tank::Hot::exit() {}
+void Tank::Hot::exit()
+{
+    m_context.m_coolOn = false;
+}
 
 // ----------------------------------------------------------------------
 // Class Tank::Normal
 // ----------------------------------------------------------------------
-int Tank::Normal::fill(int amt, int temp)
+Tank::State* Tank::Normal::fill(int temp)
 {
-    return 0;
+    m_context.m_temp = (m_context.m_temp * m_context.m_level) + temp;
+    m_context.m_level++;
+    m_context.m_temp /= m_context.m_level;
+    m_context.m_amt--;
+    return this->transition();
+}
+
+Tank::State* Tank::Normal::transition()
+{
+    if(m_context.m_level == m_context.max_level)
+        return (State*)new Tank::Full(m_context);
+    else if(m_context.m_level == m_context.min_level)
+        return (State*)new Tank::Empty(m_context);
+    else if(m_context.m_temp < m_context.m_tempMin)
+        return (State*)new Tank::Cold(m_context);
+    else if(m_context.m_temp > m_context.m_tempMax)
+        return (State*)new Tank::Hot(m_context);
+    return NULL;
 }
 
 void Tank::Normal::enter()
 {
     cout << "Tank is " << this->toString() << "\n";
 }
-
 void Tank::Normal::exit() {}
--- patternState/Tank.h
+++ patternState/Tank.h
@@ -2,7 +2,7 @@
 // Identify the different states of an object.
 // Identify the operations of an object that will be different in different states.
 // - These are the operations that the object has to delegate to state classes.
-// Identify the state transitions that need to be captured.
+// Identify transitions between states and have each state check for these.
 // If needed, implement an enter and exit method for each state to ensure that certain data is set when a state starts or finishes.
 
 #include <string>
@@ -12,9 +12,10 @@
 // class to represent a tank whose state is to be tracked
 class Tank
 {
-    public:
+    private:
         class State;        // base state class; abstract and to be inherited from
         class Empty;        // represents the state when Tank is Empty
+        class Full;         // represents the state when Tank is Full
         class Cold;         // represents the state when Tank is Cold
         class Hot;          // represents the state when Tank is Hot
         class Normal;       // represents the state when Tank is Normal
@@ -24,7 +25,7 @@ class Tank
         ~Tank();
         int temp() const {return m_temp;}
         int level() const {return m_level;}
-        void fill(int amt, int temp);       // operation that will be different at different states
+        int fill(int amt, int temp);        // operation that will be different at different states
         std::string toString();
 
     public:
@@ -34,12 +35,16 @@ class Tank
         static const int max_level = 100;
 
     private:
-        State* m_state;     // state of the tank
-        int m_temp;         // 15 - 150 [Celsius]
-        int m_level;        // 0 - 100 [%]
-        int m_tempMin;      // minimum temperature to maintain
-        int m_tempMax;      // maximum temperature to avoid
-        void updateState(); // check for state changes
+        State* m_state;                     // state of the tank
+        int m_temp;                         // 15 - 150 [Celsius]
+        int m_level;                        // 0 - 100 [%]
+        int m_tempMin;                      // minimum temperature to maintain
+        int m_tempMax;                      // maximum temperature to avoid
+        bool m_hatchOpen;                   // false - tank hatch is closed, true - tank hatch is open
+        bool m_heatOn;                      // false - heating elements off, true - heating elements on
+        bool m_coolOn;                      // false - cooling off, true - cooling on
+        void changeState(State* newState);  // for concrete state classes to use to transition to another state
+        int m_amt;                          // amount of liquid left to fill the tank
 };
 
 // abstract base class for a tank's state
@@ -48,9 +53,11 @@ class Tank::State
     public:
         State(Tank& tank) : m_context(tank) {};         // store backreference to original object
         ~State() {};
-        virtual int fill(int amt, int temp) = 0;        // pure virtual function; must be overridden in every concrete state
+        virtual State* fill(int temp) = 0;              // pure virtual function; must be overridden in every concrete state
         virtual void enter() = 0;                       // to ensure that certain data is set when entering or leaving a state
         virtual void exit() = 0;
+        virtual State* transition() = 0;                // each concrete State class must provide a method for transitioning to other states
+        virtual void fix() {};                          // each concrete State class' method to attempt to Normal state
         virtual std::string toString() = 0;
 
     protected:
@@ -62,8 +69,21 @@ class Tank::Empty : State
 {
     public:
         Empty(Tank& tank):State(tank) {};               // call base constructor to store backreference
-        std::string toString() { return std::string("Empty"); }
-        int fill(int amt, int temp) override;
+        std::string toString() {return std::string("Empty");}
+        State* fill(int temp) override;
+        // use default implementation for fix() as it does not apply to this state
+        State* transition() override;
+        void enter() override;
+        void exit() override;
+};
+
+class Tank::Full : State
+{
+    public:
+        Full(Tank& tank):State(tank) {};
+        std::string toString() {return std::string("Full");}
+        State* fill(int temp) override;
+        State* transition() override;
         void enter() override;
         void exit() override;
 };
@@ -72,8 +92,10 @@ class Tank::Cold : State
 {
     public:
         Cold(Tank& tank):State(tank) {};
-        std::string toString() { return std::string("Cold"); }
-        int fill(int amt, int temp) override;
+        std::string toString() {return std::string("Cold");}
+        State* fill(int temp) override;
+        State* transition() override;
+        void fix() override;
         void enter() override;
         void exit() override;
 };
@@ -82,8 +104,10 @@ class Tank::Hot : State
 {
     public:
         Hot(Tank& tank):State(tank) {};
-        std::string toString() { return std::string("Hot"); }
-        int fill(int amt, int temp) override;
+        std::string toString() {return std::string("Hot");}
+        State* fill(int temp) override;
+        State* transition() override;
+        void fix() override;
         void enter() override;
         void exit() override;
 };
@@ -92,8 +116,9 @@ class Tank::Normal : State
 {
     public:
         Normal(Tank& tank):State(tank) {};
-        std::string toString() { return std::string("Normal"); }
-        int fill(int amt, int temp) override;
+        std::string toString() {return std::string("Normal");}
+        State* fill(int temp) override;
+        State* transition() override;
         void enter() override;
         void exit() override;
 };
